425b1db4e580348f47e4bff47add688a19d02093
GreatPxs <1724243747@qq.com>
2024-03-08 09:41:05 +0800

    240308-ini-v0.1-p

---
+++ src/main/java/com/example/virlearning/VirlearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.virlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirlearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirlearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/virlearning/VirlearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirlearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
